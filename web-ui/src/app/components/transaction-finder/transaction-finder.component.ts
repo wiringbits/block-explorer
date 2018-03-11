198c28b95c85870369d06288589e7561e3435073
Alexis Hernandez <alexis22229@gmail.com>
2018-03-11 12:56:31 -0600

    web-ui: Load transaction details from the TransactionFinder

--- web-ui/src/app/components/transaction-finder/transaction-finder.component.ts
+++ web-ui/src/app/components/transaction-finder/transaction-finder.component.ts
@@ -3,6 +3,8 @@ import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/fo
 import { Observable } from 'rxjs/Observable';
 
 import { ErrorService } from '../../services/error.service';
+import { NavigatorService } from '../../services/navigator.service';
+import { TransactionsService } from '../../services/transactions.service';
 
 @Component({
   selector: 'app-transaction-finder',
@@ -15,6 +17,8 @@ export class TransactionFinderComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
+    private navigatorService: NavigatorService,
+    private transactionsService: TransactionsService,
     public errorService: ErrorService) {
 
     this.createForm();
@@ -30,6 +34,13 @@ export class TransactionFinderComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log('submit now!');
+    const txid = this.form.get('transactionId').value;
+
+    // instead of redirecting, we check if the transaction is valid.
+    this.transactionsService.get(txid)
+      .subscribe(
+        response => this.navigatorService.transactionDetails(txid),
+        response => this.errorService.renderServerErrors(this.form, response)
+      );
   }
 }
