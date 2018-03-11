e0ceb5acef4466d6316a84fa2ad0cc0642579473
Alexis Hernandez <alexis22229@gmail.com>
2018-03-11 12:54:44 -0600

    web-ui: Add route for TransactionDetailsComponent

--- web-ui/src/app/app-routing.module.ts
+++ web-ui/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
+import { TransactionDetailsComponent } from './components/transaction-details/transaction-details.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'transactions/:txid', component: TransactionDetailsComponent },
   { path: '**', redirectTo: '' }
 ];
 
