8eff288e336b8a6fe805a40eefa8e02361d42695
Alexis Hernandez <alexis22229@gmail.com>
2018-04-08 18:49:37 -0500

    server: Add SeederConfig

--- server/app/com/xsn/explorer/modules/ConfigModule.scala
+++ server/app/com/xsn/explorer/modules/ConfigModule.scala
@@ -1,11 +1,12 @@
 package com.xsn.explorer.modules
 
 import com.google.inject.AbstractModule
-import com.xsn.explorer.config.{PlayRPCConfig, RPCConfig}
+import com.xsn.explorer.config.{PlayRPCConfig, PlaySeederConfig, RPCConfig, SeederConfig}
 
 class ConfigModule extends AbstractModule {
 
   override def configure(): Unit = {
     bind(classOf[RPCConfig]).to(classOf[PlayRPCConfig])
+    bind(classOf[SeederConfig]).to(classOf[PlaySeederConfig])
   }
 }
