f560a8f56731ce55644333ff35c6d9e733726abc
Alexis Hernandez <alexis22229@gmail.com>
2018-04-11 23:30:05 -0500

    server: Add serializer for the Balance model

--- server/app/com/xsn/explorer/models/Balance.scala
+++ server/app/com/xsn/explorer/models/Balance.scala
@@ -1,9 +1,23 @@
 package com.xsn.explorer.models
 
+import play.api.libs.json._
+
 case class Balance(
     address: Address,
     received: BigDecimal = BigDecimal(0),
     spent: BigDecimal = BigDecimal(0)) {
 
   def available: BigDecimal = received - spent
+}
+
+object Balance {
+  implicit val writes: Writes[Balance] = Writes { obj =>
+    val values = Map(
+      "address" -> JsString(obj.address.string),
+      "received" -> JsNumber(obj.received),
+      "spent" -> JsNumber(obj.spent),
+      "available" -> JsNumber(obj.available))
+
+    JsObject.apply(values)
+  }
 }
