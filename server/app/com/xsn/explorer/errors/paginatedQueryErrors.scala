a784e60641f88b322ffe5d927d4ac8e228f4bd12
Alexis Hernandez <alexis22229@gmail.com>
2018-04-13 19:54:18 -0500

    server: Fix messages on PaginatedQueryError

--- server/app/com/xsn/explorer/errors/paginatedQueryErrors.scala
+++ server/app/com/xsn/explorer/errors/paginatedQueryErrors.scala
@@ -7,7 +7,7 @@ sealed trait PaginatedQueryError
 
 case object PaginatedQueryOffsetError extends PaginatedQueryError with InputValidationError {
   override def toPublicErrorList(messagesApi: MessagesApi)(implicit lang: Lang): List[PublicError] = {
-    val message = messagesApi("error.paginatedQuery.offset")
+    val message = messagesApi("error.paginatedQuery.offset.invalid")
     val error = FieldValidationError("offset", message)
     List(error)
   }
@@ -15,7 +15,7 @@ case object PaginatedQueryOffsetError extends PaginatedQueryError with InputVali
 
 case class PaginatedQueryLimitError(maxValue: Int) extends PaginatedQueryError with InputValidationError {
   override def toPublicErrorList(messagesApi: MessagesApi)(implicit lang: Lang): List[PublicError] = {
-    val message = messagesApi("error.paginatedQuery.limit", maxValue)
+    val message = messagesApi("error.paginatedQuery.limit.invalid", maxValue)
     val error = FieldValidationError("limit", message)
     List(error)
   }
