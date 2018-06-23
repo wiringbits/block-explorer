b32bc45d01a33ef205c46eea78032eadf8d801a8
Alexis Hernandez <alexis22229@gmail.com>
2018-06-23 09:37:44 -0500

    server: Test "GET /addresses/:address/transactions" endpoint

--- server/test/controllers/AddressesControllerSpec.scala
+++ server/test/controllers/AddressesControllerSpec.scala
@@ -1,7 +1,9 @@
 package controllers
 
 import com.alexitc.playsonify.PublicErrorRenderer
-import com.alexitc.playsonify.core.FutureApplicationResult
+import com.alexitc.playsonify.core.{ApplicationResult, FutureApplicationResult}
+import com.alexitc.playsonify.models.{Count, PaginatedQuery, PaginatedResult}
+import com.xsn.explorer.data.TransactionBlockingDataHandler
 import com.xsn.explorer.errors.AddressFormatError
 import com.xsn.explorer.helpers.{DataHelper, DummyXSNService}
 import com.xsn.explorer.models._
@@ -52,6 +54,16 @@ class AddressesControllerSpec extends MyAPISpec {
       |]
     """.stripMargin
 
+  val addressForTransactions = createAddress("XxQ7j37LfuXgsLd5DZAwFKhT3s2ZMkW86F")
+  val addressTransaction = TransactionWithValues(
+    createTransactionId("92c51e4fe89466faa734d6207a7ef6115fa1dd33f7156b006fafc6bb85a79eb8"),
+    createBlockhash("ad22f0dcea2fdaa357aac6eab00695cf07b487e34113598909f625c24629c981"),
+    12312312L,
+    Size(1000),
+    sent = 50,
+    received = 200)
+
+
   val customXSNService = new DummyXSNService {
     val map = Map(
       "Xi3sQfMQsy2CzMZTrnKW6HFGp1VqFThdLw" -> addressEmpty,
@@ -80,8 +92,26 @@ class AddressesControllerSpec extends MyAPISpec {
     }
   }
 
+  private val customTransactionDataHandler = new TransactionBlockingDataHandler {
+
+    override def upsert(transaction: Transaction): ApplicationResult[Transaction] = ???
+
+    override def delete(transactionId: TransactionId): ApplicationResult[Transaction] = ???
+
+    override def deleteBy(blockhash: Blockhash): ApplicationResult[List[Transaction]] = ???
+
+    override def getBy(address: Address, paginatedQuery: PaginatedQuery): ApplicationResult[PaginatedResult[TransactionWithValues]] = {
+      if (address == addressForTransactions) {
+        Good(PaginatedResult(paginatedQuery.offset, paginatedQuery.limit, Count(1), List(addressTransaction)))
+      } else {
+        Good(PaginatedResult(paginatedQuery.offset, paginatedQuery.limit, Count(0), List.empty))
+      }
+    }
+  }
+
   override val application = guiceApplicationBuilder
       .overrides(bind[XSNService].to(customXSNService))
+      .overrides(bind[TransactionBlockingDataHandler].to(customTransactionDataHandler))
       .build()
 
   "GET /addresses/:address" should {
@@ -123,6 +153,33 @@ class AddressesControllerSpec extends MyAPISpec {
 
       status(response) mustEqual OK
       contentAsJson(response) mustEqual Json.parse(utxosResponse)
+    }
+  }
+
+  "GET /addresses/:address/transactions" should {
+    def url(address: String, offset: Int, limit: Int) = s"/addresses/$address/transactions?offset=$offset&limit=$limit"
+
+    "return the transactions where the address was involved" in {
+      val offset = 0
+      val limit = 5
+      val response = GET(url(addressForTransactions.string, offset, limit))
+
+      status(response) mustEqual OK
+      val json = contentAsJson(response)
+      (json \ "offset").as[Int] mustEqual offset
+      (json \ "limit").as[Int] mustEqual limit
+      (json \ "total").as[Int] mustEqual 1
+
+      val data = (json \ "data").as[List[JsValue]]
+      data.size mustEqual 1
+
+      val item = data.head
+      (item \ "id").as[String] mustEqual addressTransaction.id.string
+      (item \ "blockhash").as[String] mustEqual addressTransaction.blockhash.string
+      (item \ "time").as[Long] mustEqual addressTransaction.time
+      (item \ "size").as[Int] mustEqual addressTransaction.size.int
+      (item \ "sent").as[BigDecimal] mustEqual addressTransaction.sent
+      (item \ "received").as[BigDecimal] mustEqual addressTransaction.received
     }
   }
 }
