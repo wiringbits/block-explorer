b6a98d1ffa1969c3ff9705e785ffdccbe6878600
Mario Mejia <mario128mex@gmail.com>
2018-07-23 14:09:35 -0500

    web-ui: improvements on block details view (#19)
    transactions for the block are now retrieved separately, the
    transactions table is paginated and shows more info and
    finally the view now is mobile friendly

--- web-ui/src/app/components/block-details/block-details.component.ts
+++ web-ui/src/app/components/block-details/block-details.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
-import { TranslateService } from '@ngx-translate/core';
+import { Observable } from 'rxjs/Observable';
 
 import { BlockDetails } from '../../models/block';
+import { Transaction } from '../../models/transaction';
 
 import { BlocksService } from '../../services/blocks.service';
 import { ErrorService } from '../../services/error.service';
@@ -16,7 +17,14 @@ import { NavigatorService } from '../../services/navigator.service';
 })
 export class BlockDetailsComponent implements OnInit {
 
+  blockhash: string;
   blockDetails: BlockDetails;
+
+  // pagination
+  total = 0;
+  currentPage = 1;
+  pageSize = 10;
+  asyncItems: Observable<Transaction[]>;
 
   constructor(
     private route: ActivatedRoute,
@@ -30,15 +38,38 @@ export class BlockDetailsComponent implements OnInit {
   }
 
   private onBlockhash(blockhash: string) {
-    this.blockDetails = null;
+    this.clearCurrentValues();
+    this.blockhash = blockhash;
     this.blocksService.get(blockhash).subscribe(
       response => this.onBlockRetrieved(response),
       response => this.onError(response)
     );
+    this.getPage(this.currentPage);
+  }
+
+  private clearCurrentValues() {
+    this.blockhash = null;
+    this.blockDetails = null;
+    this.total = 0;
+    this.currentPage = 1;
+    this.pageSize = 10;
+    this.asyncItems = null;
   }
 
   private onBlockRetrieved(response: BlockDetails) {
     this.blockDetails = response;
+  }
+
+  getPage(page: number) {
+    const offset = (page - 1) * this.pageSize;
+    const limit = this.pageSize;
+    const order = 'time:desc';
+
+    this.asyncItems = this.blocksService
+      .getTransactions(this.blockhash, offset, limit, order)
+      .do(response => this.total = response.total)
+      .do(response => this.currentPage = 1 + (response.offset / this.pageSize))
+      .map(response => response.data)
   }
 
   private onError(response: any) {
