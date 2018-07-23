78e36c03c83508f0706c7e8714e866e016710c54
Mario Mejia <mario128mex@gmail.com>
2018-07-23 14:09:35 -0500

    web-ui: improvements on block details view (#19)
    transactions for the block are now retrieved separately, the
    transactions table is paginated and shows more info and
    finally the view now is mobile friendly

--- web-ui/src/app/services/blocks.service.ts
+++ web-ui/src/app/services/blocks.service.ts
@@ -5,6 +5,8 @@ import { Observable } from 'rxjs/Observable';
 import { environment } from '../../environments/environment';
 
 import { Block, BlockDetails } from '../models/block';
+import { PaginatedResult } from '../models/paginated-result';
+import { Transaction } from '../models/transaction';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -25,6 +27,11 @@ export class BlocksService {
   getRaw(query: string): Observable<any> {
     const url = `${this.baseUrl}/${query}/raw`;
     return this.http.get<any>(url);
+  }
+
+  getTransactions(hash: string, offset: number = 0, limit: number = 10, orderBy: string = ''): Observable<PaginatedResult<Transaction>> {
+    const url = `${this.baseUrl}/${hash}/transactions?offset=${offset}&limit=${limit}&orderBy=${orderBy}`;
+    return this.http.get<PaginatedResult<Transaction>>(url);
   }
 
   getLatest(): Observable<Block[]> {
