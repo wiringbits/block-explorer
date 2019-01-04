54a61a11a8809c23f4606a7d73327e44900d71c2
Alexis Hernandez <alexis22229@gmail.com>
2019-01-03 20:04:08 -0700

    server: Prepare "GET /maintenance" endpoint
    This is useful for running occasional maintenance tasks.

---
+++ server/app/controllers/MaintenanceController.scala
@@ -0,0 +1,17 @@
+package controllers
+
+import controllers.common.{MyJsonController, MyJsonControllerComponents}
+import javax.inject.Inject
+import org.scalactic.Good
+import play.api.libs.json.JsObject
+
+import scala.concurrent.Future
+
+class MaintenanceController @Inject() (
+    components: MyJsonControllerComponents)
+    extends MyJsonController(components) {
+
+  def run() = public { _ =>
+    Future.successful(Good(JsObject.empty))
+  }
+}
