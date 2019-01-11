e0e2606f131b5a838b5cccfa07d19323b5236b78
jonsadev <jonsalazar99@hotmail.com>
2019-01-11 09:59:58 -0700

    web-ui: Add config file with first value currentCurrency
    Add config file in app/
    Use config value currentCurrency in explorerCurrency pipe

--- web-ui/src/app/pipes/explorer-currency.pipe.ts
+++ web-ui/src/app/pipes/explorer-currency.pipe.ts
@@ -1,10 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Config } from '../config';
 
 @Pipe({
   name: 'explorerCurrency'
 })
 export class ExplorerCurrencyPipe implements PipeTransform {
   transform(currency: any): string {
-    return `${ currency } XSN`;
+    return `${ currency } ${ Config.currentCurrency }`;
   }
 }
