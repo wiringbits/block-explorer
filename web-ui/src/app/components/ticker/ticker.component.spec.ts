2c60f25d21f4e17b8f94681a1e133d80fcb592c2
jonsadev <jonsalazar99@hotmail.com>
2019-01-21 10:25:17 -0700

    web-ui: Fix ticker.component.spec

--- web-ui/src/app/components/ticker/ticker.component.spec.ts
+++ web-ui/src/app/components/ticker/ticker.component.spec.ts
@@ -1,14 +1,33 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TickerComponent } from './ticker.component';
+import { ExplorerCurrencyPipe } from '../../pipes/explorer-currency.pipe';
+
+import { TranslateModule } from '@ngx-translate/core';
+
+import { TickerService } from '../../services/ticker.service';
+import { Observable } from 'rxjs';
 
 describe('TickerComponent', () => {
   let component: TickerComponent;
   let fixture: ComponentFixture<TickerComponent>;
 
+  const tickerServiceSpy: jasmine.SpyObj<TickerService> = jasmine.createSpyObj('TickerService', ['get']);
+
   beforeEach(async(() => {
+    tickerServiceSpy.get.and.returnValue(Observable.create());
+
     TestBed.configureTestingModule({
-      declarations: [ TickerComponent ]
+      declarations: [
+        TickerComponent,
+        ExplorerCurrencyPipe
+      ],
+      imports: [
+        TranslateModule.forRoot()
+      ],
+      providers: [
+        { provide: TickerService, useValue: tickerServiceSpy }
+      ]
     })
     .compileComponents();
   }));
