8a3669ef07d7ec78f5d8fd6dae47a9bdde1a20a6
jonsadev <jonsalazar99@hotmail.com>
2019-01-21 10:41:35 -0700

    web-ui: Fix transaction-raw.component.spec

--- web-ui/src/app/components/transaction-raw/transaction-raw.component.spec.ts
+++ web-ui/src/app/components/transaction-raw/transaction-raw.component.spec.ts
@@ -2,13 +2,39 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TransactionRawComponent } from './transaction-raw.component';
 
+import { TranslateModule } from '@ngx-translate/core';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { NavigatorService } from '../../services/navigator.service';
+import { TransactionsService } from '../../services/transactions.service';
+import { ErrorService } from '../../services/error.service';
+import { Observable } from 'rxjs';
+
+import { NO_ERRORS_SCHEMA, } from '@angular/core';
+
 describe('TransactionRawComponent', () => {
   let component: TransactionRawComponent;
   let fixture: ComponentFixture<TransactionRawComponent>;
 
+  const navigatorServiceSpy: jasmine.SpyObj<NavigatorService> = jasmine.createSpyObj('NavigatorService', ['']);
+  const transactionsServiceSpy: jasmine.SpyObj<TransactionsService> = jasmine.createSpyObj('TransactionsService', ['getRaw']);
+  const errorServiceSpy: jasmine.SpyObj<ErrorService> = jasmine.createSpyObj('ErrorService', ['renderServerErrors']);
+
   beforeEach(async(() => {
+    transactionsServiceSpy.getRaw.and.returnValue(Observable.create());
+
     TestBed.configureTestingModule({
-      declarations: [ TransactionRawComponent ]
+      declarations: [ TransactionRawComponent ],
+      imports: [
+        TranslateModule.forRoot(),
+        RouterTestingModule
+      ],
+      providers: [
+        { provide: NavigatorService, useValue: navigatorServiceSpy },
+        { provide: TransactionsService, useValue: transactionsServiceSpy },
+        { provide: ErrorService, useValue: errorServiceSpy }
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   }));
