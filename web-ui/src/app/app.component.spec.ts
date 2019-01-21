bb23dc15985fd3030d36d023509abff47c7ab078
jonsadev <jonsalazar99@hotmail.com>
2019-01-21 10:53:10 -0700

    web-ui: Fix app.component.spec

--- web-ui/src/app/app.component.spec.ts
+++ web-ui/src/app/app.component.spec.ts
@@ -1,27 +1,35 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+
+import { TranslateModule } from '@ngx-translate/core';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { DEFAULT_LANG, LanguageService } from './services/language.service';
+
+import { NO_ERRORS_SCHEMA, } from '@angular/core';
+
 describe('AppComponent', () => {
+
+  const languageServiceSpy: jasmine.SpyObj<LanguageService> = jasmine.createSpyObj('LanguageService', ['getLang']);
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [
+        TranslateModule.forRoot(),
+        RouterTestingModule
+      ],
+      providers: [
+        { provide: LanguageService, useValue: languageServiceSpy },
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
   }));
 });
