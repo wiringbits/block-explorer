7619ef348306ad47b8ca0e15fa06ea6fc523c8bc
Alexis Hernandez <alexis22229@gmail.com>
2019-02-16 21:44:08 -0700

    server: Add the persisted models package
    Includes the Block model which represents what is actually persisted.

---
+++ server/app/com/xsn/explorer/models/persisted/Block.scala
@@ -0,0 +1,19 @@
+package com.xsn.explorer.models.persisted
+
+import com.xsn.explorer.models._
+
+case class Block(
+    hash: Blockhash,
+    previousBlockhash: Option[Blockhash],
+    nextBlockhash: Option[Blockhash],
+    tposContract: Option[TransactionId],
+    merkleRoot: Blockhash,
+    size: Size,
+    height: Height,
+    version: Int,
+    time: Long,
+    medianTime: Long,
+    nonce: Long,
+    bits: String,
+    chainwork: String,
+    difficulty: BigDecimal)
