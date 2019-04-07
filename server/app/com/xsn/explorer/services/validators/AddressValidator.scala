bbe4828a0e367cb358d7e5f64911ab2fe95b216f
Alexis Hernandez <alexis22229@gmail.com>
2019-04-06 17:17:15 -0700

    server: Simplify the AddressValidator

--- server/app/com/xsn/explorer/services/validators/AddressValidator.scala
+++ server/app/com/xsn/explorer/services/validators/AddressValidator.scala
@@ -3,12 +3,10 @@ package com.xsn.explorer.services.validators
 import com.alexitc.playsonify.core.ApplicationResult
 import com.xsn.explorer.errors.AddressFormatError
 import com.xsn.explorer.models.values.Address
-import org.scalactic.{One, Or}
 
 class AddressValidator {
 
   def validate(string: String): ApplicationResult[Address] = {
-    val maybe = Address.from(string)
-    Or.from(maybe, One(AddressFormatError))
+    optional(string, AddressFormatError)(Address.from)
   }
 }
