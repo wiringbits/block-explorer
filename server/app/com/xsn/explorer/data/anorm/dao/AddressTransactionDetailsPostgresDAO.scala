0dcbce0e638f3680715932b68f461fc5049b88db
Alexis Hernandez <alexis22229@gmail.com>
2019-06-30 18:03:19 -0600

    server: Extract address related logic from the AddressTransactionDetailsPostgresDAO

--- server/app/com/xsn/explorer/data/anorm/dao/AddressTransactionDetailsPostgresDAO.scala
+++ server/app/com/xsn/explorer/data/anorm/dao/AddressTransactionDetailsPostgresDAO.scala
@@ -7,6 +7,7 @@ import com.xsn.explorer.config.ExplorerConfig
 import com.xsn.explorer.data.anorm.parsers.TransactionParsers._
 import com.xsn.explorer.models.persisted.{AddressTransactionDetails, Transaction}
 import com.xsn.explorer.models.values.TransactionId
+import com.xsn.explorer.util.TransactionAddressesHelper
 import javax.inject.Inject
 import org.slf4j.LoggerFactory
 
@@ -15,42 +16,7 @@ class AddressTransactionDetailsPostgresDAO @Inject()(explorerConfig: ExplorerCon
   private val logger = LoggerFactory.getLogger(this.getClass)
 
   def batchInsertDetails(transaction: Transaction.HasIO)(implicit conn: Connection): Option[Unit] = {
-    val outputAddressValueList = for {
-      output <- transaction.outputs
-      address <- output.addresses
-    } yield address -> output.value
-
-    val received = outputAddressValueList
-      .groupBy(_._1)
-      .mapValues { _.map(_._2).sum }
-      .map {
-        case (address, value) =>
-          AddressTransactionDetails(address, transaction.id, time = transaction.time, received = value)
-      }
-
-    val inputAddressValueList = for {
-      input <- transaction.inputs
-      address <- input.addresses
-    } yield address -> input.value
-
-    val sent = inputAddressValueList
-      .groupBy(_._1)
-      .mapValues { _.map(_._2).sum }
-      .map {
-        case (address, value) =>
-          AddressTransactionDetails(address, transaction.id, time = transaction.time, sent = value)
-      }
-
-    val details = (received ++ sent)
-      .groupBy(_.address)
-      .mapValues {
-        case head :: list =>
-          list.foldLeft(head) { (acc, current) =>
-            current.copy(received = current.received + acc.received, sent = current.sent + acc.sent)
-          }
-      }
-      .values
-
+    val details = TransactionAddressesHelper.computeDetails(transaction)
     batchInsertDetails(details.toList)
   }
 
---
+++ server/app/com/xsn/explorer/util/TransactionAddressesHelper.scala
@@ -0,0 +1,55 @@
+package com.xsn.explorer.util
+
+import com.xsn.explorer.models.persisted.{AddressTransactionDetails, Transaction}
+
+object TransactionAddressesHelper {
+
+  private def computeReceiveDetails(transaction: Transaction.HasIO): Iterable[AddressTransactionDetails] = {
+    val outputAddressValueList = for {
+      output <- transaction.outputs
+      address <- output.addresses
+    } yield address -> output.value
+
+    val received = outputAddressValueList
+      .groupBy(_._1)
+      .mapValues { _.map(_._2).sum }
+      .map {
+        case (address, value) =>
+          AddressTransactionDetails(address, transaction.id, time = transaction.time, received = value)
+      }
+
+    received
+  }
+
+  private def computeSendDetails(transaction: Transaction.HasIO): Iterable[AddressTransactionDetails] = {
+    val inputAddressValueList = for {
+      input <- transaction.inputs
+      address <- input.addresses
+    } yield address -> input.value
+
+    val sent = inputAddressValueList
+      .groupBy(_._1)
+      .mapValues { _.map(_._2).sum }
+      .map {
+        case (address, value) =>
+          AddressTransactionDetails(address, transaction.id, time = transaction.time, sent = value)
+      }
+
+    sent
+  }
+
+  def computeDetails(transaction: Transaction.HasIO): Iterable[AddressTransactionDetails] = {
+    val details = (computeReceiveDetails(transaction) ++ computeSendDetails(transaction))
+      .groupBy(_.address)
+      .mapValues {
+        case head :: list => list.foldLeft(head)(merge)
+      }
+      .values
+
+    details
+  }
+
+  private def merge(x: AddressTransactionDetails, y: AddressTransactionDetails): AddressTransactionDetails = {
+    x.copy(received = x.received + y.received, sent = x.sent + y.sent)
+  }
+}
