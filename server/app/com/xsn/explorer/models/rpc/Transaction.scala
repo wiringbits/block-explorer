5770575144b1e08a0772b2a9d4936304ad833eb2
Alexis Hernandez <alexis22229@gmail.com>
2019-08-02 17:22:09 -0600

    server: Fix missing transaction time while loading full blocks
    The full block retrieval API from xsnd assumes that the blocktime
    is the same as the transaction time.

--- server/app/com/xsn/explorer/models/rpc/Transaction.scala
+++ server/app/com/xsn/explorer/models/rpc/Transaction.scala
@@ -21,8 +21,7 @@ object Transaction {
     val builder = (__ \ 'txid).read[TransactionId] and
       (__ \ 'size).read[Size] and
       (__ \ 'blockhash).read[Blockhash] and
-      (__ \ 'time).readNullable[Long] and
-      (__ \ 'blocktime).readNullable[Long] and
+      (__ \ 'blocktime).read[Long] and
       (__ \ 'confirmations).read[Confirmations] and
       (__ \ 'vout).read[List[TransactionVOUT]] and
       (__ \ 'vin)
@@ -32,9 +31,8 @@ object Transaction {
           list.flatMap(_.asOpt[TransactionVIN])
         }
 
-    // TODO: Enfore blocktime and time fields when https://github.com/X9Developers/XSN/issues/72 is fixed.
-    builder.apply { (id, size, blockHash, time, blockTime, confirmations, vout, vin) =>
-      Transaction(id, size, blockHash, time.getOrElse(0), blockTime.getOrElse(0), confirmations, vin, vout)
+    builder.apply { (id, size, blockHash, blockTime, confirmations, vout, vin) =>
+      Transaction(id, size, blockHash, blockTime, blockTime, confirmations, vin, vout)
     }
   }
 
@@ -45,7 +43,6 @@ object Transaction {
   ): Reads[Transaction[TransactionVIN]] = {
     val builder = (__ \ 'txid).read[TransactionId] and
       (__ \ 'size).read[Size] and
-      (__ \ 'time).readNullable[Long] and
       (__ \ 'vout).read[List[TransactionVOUT]] and
       (__ \ 'vin)
         .readNullable[List[JsValue]]
@@ -54,8 +51,8 @@ object Transaction {
           list.flatMap(_.asOpt[TransactionVIN])
         }
 
-    builder.apply { (id, size, time, vout, vin) =>
-      Transaction(id, size, blockhash, time.getOrElse(0), blocktime, confirmations, vin, vout)
+    builder.apply { (id, size, vout, vin) =>
+      Transaction(id, size, blockhash, blocktime, blocktime, confirmations, vin, vout)
     }
   }
 }
