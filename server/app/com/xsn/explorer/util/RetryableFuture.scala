40c9e6ca399cc1a3b9f5a9f948e1a8dc4abc3b35
Raul Lopez <raul.hll@hotmail.com>
2019-08-25 17:11:19 -0500

    server: Implement RetryableFutures

---
+++ server/app/com/xsn/explorer/util/RetryableFuture.scala
@@ -0,0 +1,91 @@
+package com.xsn.explorer.util
+
+import scala.concurrent.{ExecutionContext, Future}
+import akka.actor.Scheduler
+import akka.pattern.Patterns.after
+
+import scala.concurrent.duration._
+import scala.util.{Random, Try}
+
+object RetryableFuture {
+
+  /**
+   * Retries a future if the result fulfills the specified conditions
+   *
+   * @param delays how much time will be waited between each retry, also @delays.length is the number of times
+   *               the future will be retried.
+   * @param shouldRetry indicates whether or not retry the future depending on the current result
+   * @param f the future to be retried
+   */
+  def apply[A](
+      delays: List[FiniteDuration]
+  )(
+      shouldRetry: Try[A] => Boolean
+  )(f: => Future[A])(implicit ec: ExecutionContext, scheduler: Scheduler): Future[A] = {
+    delays.foldLeft(f) {
+      case (result, delay) =>
+        result.transformWith { t =>
+          if (shouldRetry(t)) {
+            after(delay, scheduler, ec, Future.successful(1)).flatMap(_ => f)
+          } else {
+            result
+          }
+        }
+    }
+  }
+
+  /**
+   * creates a RetryableFuture with delays that double with each retry until max delay is reached
+   * @param initialDelay the delay for the first retry
+   * @param maxDelay the max delay that will be waited while retrying the future
+   */
+  def withExponentialBackoff[A](
+      initialDelay: FiniteDuration,
+      maxDelay: FiniteDuration
+  )(implicit ec: ExecutionContext, scheduler: Scheduler) = {
+    def generateDelays: (Long, Long, Int) => List[Long] = (initialDelay: Long, maxDelay: Long, retry: Int) => {
+      val currentDelay = getDelay(initialDelay, retry).toMillis
+      if (currentDelay > maxDelay) {
+        List.empty[Long]
+      } else {
+        currentDelay :: generateDelays(initialDelay, maxDelay, retry + 1)
+      }
+    }
+
+    val delays = generateDelays(initialDelay.toMillis, maxDelay.toMillis, 0).map(_.millis)
+    RetryableFuture[A](delays) _
+  }
+
+  /**
+   * creates a RetryableFuture with delays that double with each retry
+   * @param initialDelay the delay for the first retry
+   * @param maxRetries the number of times the future will be retried
+   */
+  def withExponentialBackoff[A](
+      initialDelay: FiniteDuration,
+      maxRetries: Int
+  )(implicit ec: ExecutionContext, scheduler: Scheduler) = {
+    val initialDelayMillis = initialDelay.toMillis
+    val delays = List.tabulate(maxRetries)(n => getDelay(initialDelayMillis, n))
+
+    RetryableFuture[A](delays) _
+  }
+
+  /**
+   * calculates the delay for the given retry number
+   * @param baseDelay the delay for the first retry
+   * @param retry the retry for which the delay is being calculated
+   * @param factor how much the delay will grow with each retry.
+   *               for example:
+   *                 2 -> double the delay with each retry
+   *                 3 -> triples the delay with each retry
+   */
+  private[util] def getDelay(
+      baseDelay: Long,
+      retry: Int,
+      factor: Int = 2,
+      jitter: Int = Random.nextInt(100)
+  ): FiniteDuration = {
+    ((Math.pow(factor, retry) * baseDelay).longValue + jitter).millis
+  }
+}
---
+++ server/test/com/xsn/explorer/util/RetryableFutureSpec.scala
@@ -0,0 +1,168 @@
+package com.xsn.explorer.util
+
+import org.scalatest.{AsyncWordSpec, BeforeAndAfterAll, MustMatchers}
+
+import scala.concurrent.Future
+import akka.actor.{ActorSystem, Scheduler}
+
+import scala.concurrent.duration._
+import scala.util.{Failure, Try}
+
+class RetryableFutureSpec extends AsyncWordSpec with MustMatchers with BeforeAndAfterAll {
+
+  override def afterAll: Unit = {
+    actorSystem.terminate()
+    ()
+  }
+
+  class UnstableOperation(numberOfFailures: Int, error: Throwable) {
+    var tryCount: Int = 0
+    var lastExecutionTime: Long = 0
+    private var delays: List[FiniteDuration] = List.empty[FiniteDuration]
+
+    def getDelays = {
+      delays.reverse
+    }
+
+    def execute: Future[Int] = {
+      if (lastExecutionTime > 0) {
+        delays = (System.currentTimeMillis - lastExecutionTime).millis :: delays
+      }
+      lastExecutionTime = System.currentTimeMillis
+
+      Future {
+        if (tryCount == numberOfFailures) {
+          1
+        } else {
+          tryCount = tryCount + 1
+          throw error
+        }
+      }
+    }
+  }
+
+  object UnstableOperation {
+
+    def apply(numberOfFailures: Int, error: Throwable): UnstableOperation = {
+      new UnstableOperation(numberOfFailures, error)
+    }
+  }
+
+  val actorSystem = ActorSystem()
+  implicit val scheduler: Scheduler = actorSystem.scheduler
+
+  private val retryOnRuntimeException = (result: Try[Int]) => {
+    result match {
+      case Failure(_: RuntimeException) => true
+      case _ => false
+    }
+  }
+
+  private val threeRetries = List.fill(3)(100.millis)
+  private val withThreeRetriesOnRuntimeException = RetryableFuture(threeRetries)(retryOnRuntimeException)(_)
+
+  "RetryableFuture" should {
+    "succeed after retrying" in {
+      val operation = UnstableOperation(3, new RuntimeException)
+
+      val result = withThreeRetriesOnRuntimeException {
+        operation.execute
+      }
+
+      result.map(_ must be(1))
+    }
+
+    "fail after too many retries" in {
+      val operation = UnstableOperation(4, new RuntimeException)
+
+      val result = withThreeRetriesOnRuntimeException {
+        operation.execute
+      }
+
+      result.failed.map(_ => 1 must be(1))
+    }
+
+    "retry only when shouldRetry returns true" in {
+      val operation = UnstableOperation(2, new Exception)
+
+      val result = withThreeRetriesOnRuntimeException {
+        operation.execute
+      }
+
+      result.failed.map(_ => 1 must be(1))
+    }
+
+    "succeed when operation does not need to be retried" in {
+      val operation = UnstableOperation(0, new Exception)
+
+      val result = withThreeRetriesOnRuntimeException {
+        operation.execute
+      }
+
+      result.map(_ must be(1))
+    }
+  }
+
+  "withExponentialBackoff" should {
+    "Calculate the right delays with initial delay and max delay" in {
+      val operation = UnstableOperation(4, new RuntimeException)
+      val retryWithExponentialBackoff = RetryableFuture.withExponentialBackoff[Int](50.millis, 500.millis)
+      val result = retryWithExponentialBackoff(retryOnRuntimeException) {
+        operation.execute
+      }
+
+      val expecetedDelays = List(50.millis, 100.millis, 200.millis, 400.millis)
+
+      result.map(
+        _ =>
+          expecetedDelays
+            .zip(operation.getDelays)
+            .map {
+              case (d1, d2) => isCloseTo(d1.toMillis, d2.toMillis)
+            }
+            .forall(a => a) must be(true)
+      )
+    }
+
+    "Calculate the right delays with initial delay and max retries" in {
+      val operation = UnstableOperation(4, new RuntimeException)
+      val retryWithExponentialBackoff = RetryableFuture.withExponentialBackoff[Int](50.millis, 4)
+      val result = retryWithExponentialBackoff(retryOnRuntimeException) {
+        operation.execute
+      }
+
+      val expecetedDelays = List(50.millis, 100.millis, 200.millis, 400.millis)
+
+      result.map(
+        _ =>
+          expecetedDelays
+            .zip(operation.getDelays)
+            .map {
+              case (d1, d2) => isCloseTo(d1.toMillis, d2.toMillis)
+            }
+            .forall(a => a) must be(true)
+      )
+    }
+  }
+
+  "getDelay" should {
+    "return base delay with retry zero" in {
+      val delay = RetryableFuture.getDelay(100, 0)
+      isCloseTo(delay.toMillis, 100) must be(true)
+    }
+
+    "should double the base delay with each retry by default" in {
+      val delay = RetryableFuture.getDelay(100, 2)
+      isCloseTo(delay.toMillis, 400) must be(true)
+    }
+
+    "should grow the base delay according to the supplied factor" in {
+      val delay = RetryableFuture.getDelay(100, 2, factor = 4)
+      isCloseTo(delay.toMillis, 1600) must be(true)
+    }
+  }
+
+  private def isCloseTo(n1: Long, n2: Long, delta: Long = 130) = {
+    Math.abs(n1 - n2) <= delta
+  }
+}
