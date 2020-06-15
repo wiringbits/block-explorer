d4fb78a306b1c7105dfb39318dbd42527bc7bab0
Alexis Hernandez <alexis22229@gmail.com>
2020-06-14 19:18:49 -0600

    server: scalafmt

--- server/app/com/xsn/explorer/models/MarketStatistics.scala
+++ server/app/com/xsn/explorer/models/MarketStatistics.scala
@@ -9,11 +9,12 @@ case class MarketStatistics(prices: Map[Currency, BigDecimal], marketInformation
 
 object MarketStatistics {
   implicit val writes: Writes[MarketStatistics] = (marketStatistics: MarketStatistics) => {
-    val values = marketStatistics.prices.map {
-      case (currency, price) => currency.entryName.toLowerCase -> price
-    }
-    .updated("volume", marketStatistics.marketInformation.volume.setScale(8, RoundingMode.HALF_UP))
-    .updated("marketcap", marketStatistics.marketInformation.marketcap.setScale(8, RoundingMode.HALF_UP))
+    val values = marketStatistics.prices
+      .map {
+        case (currency, price) => currency.entryName.toLowerCase -> price
+      }
+      .updated("volume", marketStatistics.marketInformation.volume.setScale(8, RoundingMode.HALF_UP))
+      .updated("marketcap", marketStatistics.marketInformation.marketcap.setScale(8, RoundingMode.HALF_UP))
 
     Json.toJson(values)
   }
