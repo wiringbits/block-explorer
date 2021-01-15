a04af24cfb86e4db8f4b7b9410a43c868ad47fb3
Alexis Hernandez <alexis22229@gmail.com>
2021-01-15 15:04:25 -0700

    web-ui: Add a higher fee option for trezor transactions
    This is a workaround for big transactions that need higher fees.

--- web-ui/src/app/models/fees.ts
+++ web-ui/src/app/models/fees.ts
@@ -6,6 +6,7 @@ export class Fee {
 }
 
 export const TransactionFees = [
+    new Fee('Very high (use this if the transaction is failing)', 10000),
     new Fee('High', 1000),
     new Fee('Normal', 500),
     new Fee('Low', 100)
