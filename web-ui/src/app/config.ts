4107a5c3a181c371cedf6c986c6d3720571e39ac
Adinael Perez Ruelas <adin692@gmail.com>
2021-02-01 23:58:12 -0700

    web-ui: Add the label for the new cryptopia address

--- web-ui/src/app/config.ts
+++ web-ui/src/app/config.ts
@@ -3,8 +3,9 @@ export class Config {
 }
 
 export const addressLabels = {
-    '7jqffPwhzkVGbYFf525muhas6uVrhERwAm': 'Cryptopia',
+    '7jqffPwhzkVGbYFf525muhas6uVrhERwAm': 'Cryptopia old',
     '7iYWrbBhYczSYPZ3Naesd9Yk4X5qn4dNAg': 'Livecoin',
     'Xi1BEVHFRYg1QQfrNJF8L9yXvREAADxCLk': 'Stakenet.io Cloud Staking',
-    'XmPe9BHRsmZeThtYF34YYjdnrjmcAUn8bC': 'Burned coins'
+    'XmPe9BHRsmZeThtYF34YYjdnrjmcAUn8bC': 'Burned coins',
+    'xc1qm82zpjy4ysapdrfr3v7fzphc43q9u4sd20t3ud': 'Cryptopia new'
 };
