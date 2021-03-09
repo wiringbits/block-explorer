0deff1afd08b9101167c2e4dda072925d1c135e6
midasdev711 <midasdev711@gmail.com>
2021-03-09 14:03:31 -0500

    fix cors issue in dex-monitor api call

--- web-ui/src/app/services/orderbook.service.ts
+++ web-ui/src/app/services/orderbook.service.ts
@@ -6,8 +6,9 @@ import { environment } from '../../environments/environment';
 
 import { NodesInfo, TradesNumber, Volume } from '../models/orderbook';
 
-const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+const HTTPOPTIONS = {
+    exposeHeaders: 'Access-Control-Allow-Origin',
+    headers: new HttpHeaders({'Content-Type': 'application/json'})
 };
 
 @Injectable()
@@ -19,16 +20,16 @@ export class OrderBookService {
 
   getTradesNumber(subUrl: string, query: string): Observable<TradesNumber> {
     const url = `${this.baseUrl}/trading-pairs/${subUrl}/trades-number${query}`;
-    return this.http.get<TradesNumber>(url);
+    return this.http.get<TradesNumber>(url, HTTPOPTIONS);
   }
 
   getNodesInfo(subUrl: string): Observable<NodesInfo> {
     const url = `${this.baseUrl}/trading-pairs/${subUrl}/nodes-info`;
-    return this.http.get<NodesInfo>(url);
+    return this.http.get<NodesInfo>(url, HTTPOPTIONS);
   }
 
   getVolume(subUrl: string, query: string): Observable<Volume> {
     const url = `${this.baseUrl}/trading-pairs/${subUrl}/volume${query}`;
-    return this.http.get<Volume>(url);
+    return this.http.get<Volume>(url, HTTPOPTIONS);
   }
 }
