23d9d818da4c73f456f0531b4c659788e1680da1
midasdev711 <midasdev711@gmail.com>
2021-03-15 11:57:05 -0400

    update pagination and fix issue in node ticker

--- web-ui/src/app/components/nodes/nodes.component.ts
+++ web-ui/src/app/components/nodes/nodes.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-nodes',
-    templateUrl: './nodes.component.html'
+    templateUrl: './nodes.component.html',
+    styleUrls: ['./nodes.component.css']
 })
 export class NodesComponent { }
