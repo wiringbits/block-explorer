301d56473a688b80e54106f518cf4516c25d80ec
Paul Quintero <paul.alonzoqr@gmail.com>
2022-04-21 23:02:35 -0600

    Add banner wiringbits

--- web-ui/src/app/components/shared/footer/footer.component.ts
+++ web-ui/src/app/components/shared/footer/footer.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
+  isCollapsed = true;
+  isClosed = true;
   constructor() { }
 
   ngOnInit() {
   }
-
 }
